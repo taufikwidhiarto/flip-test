a4b960c01dc5d978ff960ac304b51a9ba06ae11c
taufik widhiarto <taufikwidhiarto@gmail.com>
2021-04-29 23:09:06 +0700

    added transaction status component

---
+++ src/components/status/status.component.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./status.module.css";
+
+export const TransactionStatus = ({
+  status,
+}: {
+  status: string;
+}): JSX.Element => {
+  let view = <div>{status}</div>;
+  if (status === "SUCCESS") {
+    view = <div className={styles.Success}>{"Berhasil"}</div>;
+  } else if (status === "PENDING") {
+    view = <div className={styles.Pending}>{"Pengecekan"}</div>;
+  }
+
+  return <div className={styles.StatusWrapper}>{view}</div>;
+};
